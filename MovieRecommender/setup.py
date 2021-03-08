c56484694343c7a2cc3db74250b124540c34857c
sharmeelee <48166985+sharmeelee@users.noreply.github.com>
2021-03-07 16:24:50 -0800

    Add files via upload

---
+++ MovieRecommender/setup.py
@@ -0,0 +1,12 @@
+$ pip install .
+
+
+from setuptools import setup 
+setup( 
+	name='MovieRecommender', 
+	version='1.0', 
+	description='A program that recommends movies.', 
+	author=['Priyanka Bijlani', 'Sharmeelee Bijlani', 'Laura Thriftwood', 'Lakshmi Venkatasubramanian'], 
+	packages=['MovieRecommender'], #same as name 
+	install_requires=['torch', 'numpy', 'os', 'sys', 'time', 'pandas', 'zipfile', 'tarfile', 'requests', 'matplotlib.pyplot', 'scipy.sparse', 'implicit', 'pandas.api.types', 'random', 'sklearn', 'math'], #external packages as dependencies 
+)
