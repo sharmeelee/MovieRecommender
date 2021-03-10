425f3b481794ff1aef45062e71c739f07541690c
sharmeelee <48166985+sharmeelee@users.noreply.github.com>
2021-03-09 17:07:44 -0800

    Add files via upload

---
+++ MovieRecommender/extract_data.py
@@ -0,0 +1,77 @@
+import torch
+import numpy as np
+import os, sys, time 
+import pandas as pd
+import zipfile, tarfile, requests
+import matplotlib.pyplot as plt
+
+def read_data_ml100k():
+    data_url = 'http://files.grouplens.org/datasets/movielens/ml-100k.zip'
+    def download_and_extract_data(url):
+        """Download and extract a zip/tar file."""
+        directory = './data'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        fname = os.path.join('./data', url.split('/')[-1])
+        
+        if os.path.exists(fname):
+            print(f'File {fname} already exists. Reading it')
+        else:
+            print(f'Downloading {fname} from {url}...')
+            r = requests.get(url, stream=True, verify=True)
+            with open(fname, 'wb') as f:
+                f.write(r.content)
+
+        base_dir = os.path.dirname(fname)
+        data_dir, ext = os.path.splitext(fname)
+        if ext == '.zip':
+            fp = zipfile.ZipFile(fname, 'r')
+        elif ext in ('.tar', '.gz'):
+            fp = tarfile.open(fname, 'r')
+        else:
+            assert False, 'Only zip/tar files can be extracted.'
+        fp.extractall(base_dir)
+        print('Done!')
+        return data_dir
+
+    data_dir = download_and_extract_data(data_url)
+    names = ['user_id', 'item_id', 'rating', 'timestamp']
+    data = pd.read_csv(os.path.join(data_dir, 'u.data'), '\t', names=names,engine='python')
+    movies = pd.read_csv(os.path.join(data_dir, 'u.item'), '\t', names=['movies'],engine='python')
+    num_users = data.user_id.unique().shape[0]
+    num_items = data.item_id.unique().shape[0]
+    return data, movies, num_users, num_items
+
+################################################################################
+
+def get_movies_ratings():
+    data, movies, num_users, num_items = read_data_ml100k()
+    res=[]
+    id=[]
+    for row in movies['movies']:
+        movie_id = row.split('|')[0]
+        id.append(int(movie_id))
+        movie = row.split('|')[1]
+        movie = movie.split('(')[0]
+        res.append(movie)
+    return data,pd.DataFrame({"item_id":id,"name":res},columns=["item_id","name"]),num_users, num_items
+
+###########################################################################################   
+    
+def main():
+  data,movies,num_users, num_items = get_movies_ratings()
+  directory = './output'
+  if not os.path.exists(directory):
+    os.makedirs(directory)
+  data.to_pickle("./output/ratings.pkl")
+  movies.to_pickle("./output/movies.pkl")
+
+if __name__ =="__main__":
+  data,movies,num_users, num_items = get_movies_ratings()
+  directory = './output'
+  if not os.path.exists(directory):
+    os.makedirs(directory)
+  data.to_pickle("./output/ratings.pkl")
+  movies.to_pickle("./output/movies.pkl")
+  
+
