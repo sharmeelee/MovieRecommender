a1c9f8bf63ddc66cd6788a60e4b8ed7af8ba1b12
sharmeelee <48166985+sharmeelee@users.noreply.github.com>
2021-03-12 17:27:29 -0800

    Update test_create_sparse_matrix.py

--- tests/test_create_sparse_matrix.py
+++ tests/test_create_sparse_matrix.py
@@ -2,7 +2,7 @@ import unittest
 import os
 import pickle
 import pandas as pd
-from process_data import *
+import process_data
 
 testdata = {'user_id':[1, 2, 5, 5, 5], 'item_id':[4, 5, 6, 7, 7], 'rating':[7, 1, 3, 4, 5]}
 testdf = pd.DataFrame(testdata)
