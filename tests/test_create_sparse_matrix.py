a804562f7806b63e6528139bc01ce00ad042b032
pribij <72546509+pribij@users.noreply.github.com>
2021-03-12 17:42:45 -0800

    Update test_create_sparse_matrix.py

--- tests/test_create_sparse_matrix.py
+++ tests/test_create_sparse_matrix.py
@@ -2,7 +2,7 @@ import unittest
 import os
 import pickle
 import pandas as pd
-from MovieRecommender import process_data
+from MovieRecommender import process_data.create_sparse_matrix
 
 testdata = {'user_id':[1, 2, 5, 5, 5], 'item_id':[4, 5, 6, 7, 7], 'rating':[7, 1, 3, 4, 5]}
 testdf = pd.DataFrame(testdata)
