6d88fd6d7427f3f59000974ff7474470a8932384
Lakshmi Venkat <lakshmi2688@gmail.com>
2021-03-14 17:35:57 -0700

    modified data_matrix in process_data

--- MovieRecommender/process_data.py
+++ MovieRecommender/process_data.py
@@ -51,7 +51,7 @@ def process_data(data):
 ########################################################################################### 
 
 def create_sparse_matrix(data):
-    data_matrix = data.copy()
+    data_matrix = data.loc[data.rating != 0]
 
     users = list(np.sort(data_matrix.user_id.unique())) # Get our unique users
     items = list(np.sort(data_matrix.item_id.unique())) # Get our unique movies 
