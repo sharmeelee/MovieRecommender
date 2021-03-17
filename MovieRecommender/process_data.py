6b79d355ec6e7c8511e1ba3560f360b1482331c7
sharmeelee <48166985+sharmeelee@users.noreply.github.com>
2021-03-17 12:30:05 -0700

    Update process_data.py
    flake8

--- MovieRecommender/process_data.py
+++ MovieRecommender/process_data.py
@@ -5,9 +5,10 @@ from pandas.api.types import CategoricalDtype
 from scipy.sparse import csr_matrix, save_npz
 import os
 import matplotlib.pyplot as plt
+
     
 def process_data(data):
-    data.drop('timestamp',inplace=True,axis=1,errors='ignore')
+    data.drop('timestamp', inplace=True, axis=1, errors='ignore')
     # Seeing the distribution of ratings given by the users
     p = data.groupby('rating')['rating'].agg(['count'])
     # get movie count
@@ -19,7 +20,7 @@ def process_data(data):
     ax = p.plot(kind='barh', legend=False, figsize=(15,10))
     plt.title('Total pool: {:,} Movies, {:,} customers, {:,} ratings given'.format(movie_count, cust_count, rating_count), fontsize=20)
     plt.axis('off')
-    for i in range(1,6):
+    for i in range(1, 6):
         ax.text(p.iloc[i-1][0]/4, i-1, 'Rated {}: {:.0f}%'.format(i, p.iloc[i-1][0]*100 / p.sum()[0]), color='white', weight='bold', fontsize=15)
 
     directory = 'images'
@@ -28,20 +29,22 @@ def process_data(data):
     plt.savefig('images/DistributionOfRatings.png')
     return data, cust_count, movie_count
 
+
 def create_sparse_matrix(data):
     data_matrix = data.loc[data.rating != 0]
-    users = list(np.sort(data_matrix.user_id.unique())) # Get our unique users
-    items = list(np.sort(data_matrix.item_id.unique())) # Get our unique movies 
-    rating = list(data_matrix.rating) # All of our ratings
-    rows = data_matrix.user_id.astype(CategoricalDtype(categories=users)).cat.codes 
+    users = list(np.sort(data_matrix.user_id.unique()))  # Get our unique users
+    items = list(np.sort(data_matrix.item_id.unique()))  # Get our unique movies
+    rating = list(data_matrix.rating)  # All of our ratings
+    rows = data_matrix.user_id.astype(CategoricalDtype(categories=users)).cat.codes
     cols = data_matrix.item_id.astype(CategoricalDtype(categories=items)).cat.codes
     # The implicit library expects data as a item-user matrix so we
-    # create two matricies, one for fitting the model (item-user) 
+    # create two matricies, one for fitting the model (item-user)
     # and one for recommendations (user-item)
     sparse_item_user = csr_matrix((rating, (cols, rows)), shape=(len(items), len(users)))
     sparse_user_item = csr_matrix((rating, (rows, cols)), shape=(len(users), len(items)))
-    print("Sparse matrices created : sparse_item_user ", sparse_item_user.shape, "sparse_user_item" ,sparse_user_item.shape)
+    print("Sparse matrices created : sparse_item_user ", sparse_item_user.shape, "sparse_user_item", sparse_user_item.shape)
     return sparse_item_user,sparse_user_item
+
 
 def main():
   extract_data.main()
