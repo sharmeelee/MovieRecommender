0c7069a88e5b40e3f77bf293357069df95212cfd
lakshmi2688 <lakshmi2688@gmail.com>
2021-03-17 23:40:47 -0700

    Update train_test_model.py

--- MovieRecommender/train_test_model.py
+++ MovieRecommender/train_test_model.py
@@ -7,38 +7,37 @@ from sklearn import metrics
 import os
 import pickle
 
-
-def test_train_split(sparse_user_item, pct_test=0.2):
+def test_train_split(sparse_user_item,pct_test = 0.2):
     '''
-    This function will take in the original user-item matrix and "mask" a
-    percentage of the original ratings where a user-item interaction has
-    taken place for use as a test set. The test set will contain all of
-    the original ratings, while the training set replaces the specified
-    percentage of them with a zero in the original ratings matrix.
-
-    parameters:
-
-    sparse_user_item - the original ratings sparse_user_item matrix from
+    This function will take in the original user-item matrix and "mask" a 
+    percentage of the original ratings where a user-item interaction has 
+    taken place for use as a test set. The test set will contain all of 
+    the original ratings, while the training set replaces the specified 
+    percentage of them with a zero in the original ratings matrix. 
+    
+    parameters: 
+    
+    sparse_user_item - the original ratings sparse_user_item matrix from 
     which you want to generate a train/test set. Test is just a complete
-    copy of the original set. This is in the form of a sparse csr_matrix.
-
-    pct_test - The percentage of user-item interactions where an
-    interaction took place that you want to mask in the training set for
-    later comparison to the test set, which contains all of the original
-    ratings.
-
+    copy of the original set. This is in the form of a sparse csr_matrix. 
+    
+    pct_test - The percentage of user-item interactions where an 
+    interaction took place that you want to mask in the training set for 
+    later comparison to the test set, which contains all of the original 
+    ratings. 
+    
     returns:
-
-    train_data - The altered version of the original data with a certain
-    percentage of the user-item pairs
+    
+    train_data - The altered version of the original data with a certain 
+    percentage of the user-item pairs 
     that originally had interaction set back to zero.
-
-    test_data - A copy of the original ratings matrix, unaltered, so it
-    can be used to see how the rank order compares with the actual
+    
+    test_data - A copy of the original ratings matrix, unaltered, so it 
+    can be used to see how the rank order compares with the actual 
     interactions.
-
-    users_altered  - From the randomly selected user-item indices, which
-    user rows were altered in the training data.This will be necessary
+    
+    users_altered  - From the randomly selected user-item indices, which 
+    user rows were altered in the training data.This will be necessary 
     later when evaluating the performance via AUC.
     '''
     ratings = sparse_user_item
@@ -70,7 +69,42 @@ def test_train_split(sparse_user_item, pct_test=0.2):
     return train_data, test_data, users_altered
 
 
-def train_model(train_data):
+def train_model(train_data):   
+    '''
+    Implicit weighted ALS taken from Hu, Koren, and Volinsky 2008. Designed
+    for alternating least squares and implicit feedback based collaborative
+    filtering. 
+    
+    parameters:
+    
+    train_data - Our matrix of ratings with shape m x n, where m is the
+    number of users and n is the number of items.Should be a sparse csr
+    matrix to save space. 
+    
+    regularization - Used for regularization during alternating least
+    squares. Increasing this value may increase bias but decrease
+    variance. Default is 0.1. 
+    
+    alpha_val - The parameter associated with the confidence matrix,
+    where Cui = 1 + alpha*Rui. The paper found a default of 40 most
+    effective. Decreasing this will decrease the variability in
+    confidence between various ratings.
+    
+    iterations - The number of times to alternate between both user 
+    feature vector and item feature vector in alternating least squares.
+    More iterations will allow better convergence at the cost of increased 
+    computation. We will use 50 iterations for better convergence. 
+    
+    factors - The number of latent features in the user/item feature vectors.
+    Increasing the number of features may overfit but could reduce bias. 
+    
+    returns:
+    
+    The model and feature vectors for users and items. The dot product of 
+    these feature vectors should give you the expected 
+    "rating" at each point in your original matrix.
+    
+    '''
     # Initialize the als model and fit it using the sparse item-user matrix
     als_model = implicit.als.AlternatingLeastSquares(factors=20,
                                                      regularization=1e-3,
@@ -87,9 +121,48 @@ def train_model(train_data):
           user_vecs.shape, item_vecs.shape)
     return als_model, user_vecs, item_vecs
 
-
-def evaluate_model(training_set, altered_users, predictions, test_set):
+def evaluate_model(train_data, users_altered, predictions, test_data):
+    '''
+    This function will calculate the mean AUC by user for any user that had 
+    their user-item matrix altered. 
+    
+    parameters:
+    
+    train_data - The training set resulting from make_train, where a certain 
+    percentage of the original user/item interactions are reset to zero to 
+    hide them from the model 
+    
+    predictions - The matrix of your predicted ratings for each user/item 
+    pair as output from the implicit MF. These should be stored in a list, 
+    with user vectors as item zero and item vectors as item one. 
+    
+    users_altered - The indices of the users where at least one user/item 
+    pair was altered from make_train function
+    
+    test_data - The test set constucted earlier from make_train function    
+    
+    returns:
+    
+    The mean AUC (area under the Receiver Operator Characteristic curve) of 
+    the test set only on user-item interactions there were originally zero to 
+    test ranking ability in addition to the most popular items as a benchmark.
+    '''
     def auc_score(predictions, test):
+        '''
+        This simple function will output the area under the curve using 
+        sklearn's metrics. 
+
+        parameters:
+
+        - predictions: your prediction output
+
+        - test: the actual target result you are comparing to
+
+        returns:
+
+        - AUC (area under the Receiver Operating Characterisic curve)
+        
+        '''
         fpr, tpr, thresholds = metrics.roc_curve(test, predictions)
         return metrics.auc(fpr, tpr)
     # Store AUC for each user that had item removed from training set
