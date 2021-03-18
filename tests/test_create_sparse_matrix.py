3b5039ff50f3d3f71f39e09c2b99b3db4ad30a36
pribij <72546509+pribij@users.noreply.github.com>
2021-03-17 22:00:08 -0700

    Update test_create_sparse_matrix.py
    flake8

--- tests/test_create_sparse_matrix.py
+++ tests/test_create_sparse_matrix.py
@@ -1,34 +1,37 @@
 import unittest
-import os
-import pickle
 import pandas as pd
 from MovieRecommender.process_data import create_sparse_matrix
 
-testdata = {'user_id':[1, 2, 5, 5, 5], 'item_id':[4, 5, 6, 7, 7], 'rating':[7, 1, 3, 4, 5]}
+testdata = {'user_id': [1, 2, 5, 5, 5], 'item_id': [4, 5, 6, 7, 7],
+            'rating': [7, 1, 3, 4, 5]}
 testdf = pd.DataFrame(testdata)
 testsparse_item_user, testsparse_user_item = create_sparse_matrix(testdf)
 
+
 class TestSparseMatrix(unittest.TestCase):
 
+    # check the dimensions of the returned item_user matrix
     def test_user_item(self):
-        """Check the dimensions of the returned item_user matrix"""
         returnshape = testsparse_item_user.get_shape()
-        testshape = ((testdf['item_id'].nunique()),(testdf['user_id'].nunique()))
+        testshape = ((testdf['item_id'].nunique()),
+                     (testdf['user_id'].nunique()))
         self.assertEqual(testshape, returnshape)
 
+    # check the dimensions of the returned user_item matrix
     def test_item_user(self):
-        """Check the dimensions of the returned user_item matrix"""
         returnshape = testsparse_user_item.get_shape()
-        testshape = ((testdf['user_id'].nunique()),(testdf['item_id'].nunique()))
+        testshape = ((testdf['user_id'].nunique()),
+                     (testdf['item_id'].nunique()))
         self.assertEqual(testshape, returnshape)
 
+    # check to ensure returns csr matrix format
     def test_return_format1(self):
-        """Check to ensure item_user csr matrix is returned"""
         self.assertEqual(testsparse_item_user.getformat(), "csr")
-        
+
+    # check to ensure returns csr matrix format
     def test_return_format2(self):
-        """Check to ensure user_item csr matrix is returned"""
         self.assertEqual(testsparse_user_item.getformat(), "csr")
+
 
 if __name__ == '__main__':
     unittest.main()
